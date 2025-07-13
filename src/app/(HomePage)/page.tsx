f49dc1af154c229fb799fb6c806ad332a75a940d
gibeom0218 <gb3187@naver.com>
2025-07-13 17:33:56 +0900

    feat : favicon 설정 및 메인 페이지 메타데이터 설정

--- src/app/(HomePage)/page.tsx
+++ src/app/(HomePage)/page.tsx
@@ -1,7 +1,15 @@
-'use client';
 import { Header } from './_components/header';
 import { Introduction } from './_components/introduce';
 import { PageLinks } from './_components/page-links';
+
+export const metadata = {
+  title: 'ShadcnUI Study',
+  description:
+    'This is a study page for exploring the features and components of shadcnUI',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function Home() {
   return (
